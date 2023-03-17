10147deacb59d273ca5df4363d019104a82f104f
ChuchoCarmona <108752723+ChuchoCarmona@users.noreply.github.com>
2023-03-17 10:40:17 -0600

    requerimientos

--- BDTutoriasAPI/settings.py
+++ BDTutoriasAPI/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'TutoriasBD',
         'USER': 'postgres',
-        'PASSWORD': 'admin',
+        'PASSWORD': 'holaktalA1',
         'HOST': 'localhost',
         'PORT': '5432'
     }
